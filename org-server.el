20a36b09c896320f12c7f62e065600574c097296
Erik Price <erik.price16@gmail.com>
2013-03-13 21:12:22 -0500

    Set up some basic functionality.

--- org-server.el
+++ org-server.el
@@ -24,6 +24,81 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;; Code goes here
+(require 'elnode)
+
+;; Some helper functions
+
+;; Ripped from http://emacswiki.org/emacs/ElispCookbook#toc4
+(defun string/--starts-with (s arg)
+  "returns non-nil if string S starts with ARG.  Else nil."
+  (cond ((>= (length s) (length arg))
+         (string-equal (substring s 0 (length arg)) arg))
+        (t nil)))
+
+(defgroup org-server nil
+  "A server for a directory of org-files"
+  :group 'applications)
+
+(defcustom org-server-port 8765
+  "Default port to server from"
+  :group 'org-server)
+
+(defcustom org-server-host "localhost"
+  "Default host for the server"
+  :group 'org-server)
+
+(defcustom org-server-layout-file "org-server-template.html"
+  "Template to use for the generated HTML.
+
+Files should be simple HTML, with two mustache style variables,
+{{{navigation}}} and {{{content}}} where the generated navigation bar and HTML
+generated from the Org file should go, respectively."
+  :group 'org-server)
+
+(defvar org-server--org-directory nil
+  "Directory where we're looking for Org files")
+
+(defvar org-server--org-files '()
+  "Them files we're interested in.
+
+This is an alist of file -> mtime (So generated content can be cached)")
+
+(defun org-server--org-handler (httpcon)
+  (elnode-docroot-for org-server--org-directory
+    with org-file
+    on httpcon
+    do (with-current-buffer (find-file-noselect org-file)
+         (let ((org-html (org-export-as-html 3 nil nil 'string)))
+           (elnode-send-html httpcon org-html)))))
+
+(defun org-server--build-file-list (directory)
+  "Build up a list of org-mode files in the given directory."
+  (dolist (file/attr (directory-files-and-attributes directory))
+    (let* ((file  (first file/attr))
+           (mtime  (nth 5 file/attr))
+           (ext    (file-name-extension file))
+           (path   (expand-file-name file directory)))
+
+      ;; Ignore dotfiles, ., .., and other such nonsense.
+      ;; TODO: This behavior should definitely be configurable.
+      (unless (string/--starts-with file ".")
+
+        (if (file-directory-p path)
+            (org-server--build-file-list path)
+
+          (when (string= ext "org")
+            (add-to-list 'org-server--org-files (cons path mtime))))))))
+
+(defun org-server-init (directory)
+  "Starts up a server for the given directory of org files"
+  (interactive "D")
+
+  (setq org-server--org-directory directory)
+  (setq org-server--org-files '())
+
+  (org-server--build-file-list directory)
+
+  (elnode-start 'org-server--org-handler :port org-server-port
+                :host org-server-host))
 
 ;;; org-server.el ends here
