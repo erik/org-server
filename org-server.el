5c305be8b935c4f5265c8b3adef0537e209b45da
Erik Price <erik.price16@gmail.com>
2013-03-13 19:32:55 -0500

    Initial commit.

---
+++ org-server.el
@@ -0,0 +1,29 @@
+;;; org-server.el --- Org Server application
+
+;; Copyright 2013 Erik Price
+
+;; Author: Erik Price <erik@erikprice.net>
+;; URL: http://github.com/boredomist/org-server
+;; Version: 0.0.1
+;; Created: 13th March 2013
+;; Keywords: http, org
+;; Package-Requires: ((elnode "0.9.9.6.7"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Code goes here
+
+;;; org-server.el ends here
